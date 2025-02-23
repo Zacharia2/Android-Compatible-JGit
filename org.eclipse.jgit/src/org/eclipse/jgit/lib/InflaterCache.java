9d1ab2db89dd0f8b43e8e4c4e10f1402f74c9f49
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2025-02-23 15:30:23 +0800

    try Suitable Android Inflater

--- org.eclipse.jgit/src/org/eclipse/jgit/lib/InflaterCache.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/lib/InflaterCache.java
@@ -11,6 +11,7 @@
 
 package org.eclipse.jgit.lib;
 
+import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
 /**
@@ -37,7 +38,7 @@ public class InflaterCache {
 	 */
 	public static Inflater get() {
 		final Inflater r = getImpl();
-		return r != null ? r : new Inflater(false);
+		return r != null ? r : new SuitableAndroidInflater(false);
 	}
 
 	private static synchronized Inflater getImpl() {
@@ -74,5 +75,46 @@ public class InflaterCache {
 
 	private InflaterCache() {
 		throw new UnsupportedOperationException();
+	}
+}
+
+class SuitableAndroidInflater extends Inflater {
+
+	private static final byte[] oneByteArray = new byte[1];
+
+	public SuitableAndroidInflater(boolean b) {
+		super(b);
+	}
+
+	@SuppressWarnings("nls")
+	@Override
+	public int inflate(byte[] b, int off, int len) throws DataFormatException {
+		if (len != 0) {
+			return super.inflate(b, off, len);
+		}
+
+		int bytesInflated = super.inflate(oneByteArray, 0, 1);
+		// have to pretend to want at least one byte so that
+		// the finished flag is correctly set
+		if (bytesInflated > 0) {
+			throw new RuntimeException(
+					"The Harmony-Fix hack has served you ill, we were not supposed to read any data...");
+		}
+		return 0;
+	}
+
+	@Override
+	public void reset() {
+		// Log.d(TAG,this+" - reset()");
+		// Thread.dumpStack();
+		super.reset();
+	}
+
+	@Override
+	public void end() {
+		// Log.d(TAG, this + " - end() called, will ignore");
+		// Thread.dumpStack();
+		// DO NOT call end method on wrapped inflater, because the InflaterCache
+		// will want to re-use it
 	}
 }
